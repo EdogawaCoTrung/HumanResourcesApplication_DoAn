bcce16f3440afd71c98e9aa46359e9654f049f45
tinhpham2004 <boydzpro3032004@gmail.com>
2023-11-24 17:31:06 +0700

    leaveRequestViewModelAdmin Binding Datagrid

---
+++ HumanResourcesApplication_DoAn/ViewModel/Admin/LeaveRequestsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using HumanResourcesApplication_DoAn.Model;
+using HumanResourcesApplication_DoAn.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourcesApplication_DoAn.ViewModel.Admin
+{
+    public class LeaveRequestsViewModel : ViewModelBase
+    {
+        private IListLeaveRequestsRepository? leaveRequestsRepository;
+        private List<LeaveRequest>? listLeaveRequests;
+        public List<LeaveRequest>? ListLeaveRequests { get => listLeaveRequests; set { listLeaveRequests = value; OnPropertyChanged(nameof(ListLeaveRequests)); } }
+
+        public LeaveRequestsViewModel()
+        {
+            leaveRequestsRepository = new LeaveRequestsRepository();
+            ListLeaveRequests = new List<LeaveRequest>();
+            ListLeaveRequests = leaveRequestsRepository.ListLeaveRequests();
+        }
+    }
+}
