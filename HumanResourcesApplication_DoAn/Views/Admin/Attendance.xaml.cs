f194934382ef4fe19be20ebb746f4de068a7317b
EdogawaCoTrung <quangtrungproki@gmail.com>
2023-11-27 07:40:24 +0700

    add all

--- HumanResourcesApplication_DoAn/Views/Admin/Attendance.xaml.cs
+++ HumanResourcesApplication_DoAn/Views/Admin/Attendance.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HumanResourcesApplication_DoAn.Model;
+using HumanResourcesApplication_DoAn.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,30 @@ namespace HumanResourcesApplication_DoAn.Views.Admin
     /// </summary>
     public partial class Attendance : UserControl
     {
+        private IListAttendanceRepository? attendanceRepository;
         public Attendance()
         {
             InitializeComponent();
+            attendanceRepository = new ListAttendanceRepository();
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(Attendance_Users);
+            view.Filter = UserFilter;
+        }
+
+        private bool UserFilter(object obj)
+        {
+            bool canFilter = false;
+            if (string.IsNullOrEmpty(Attendancetxtfilter.Text))
+                canFilter=true;
+            else if (obj != null)
+            {
+                canFilter = ((obj as Attendance)?.Name.IndexOf(Attendancetxtfilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+                
+            }
+            return canFilter;
+        }
+        private void AttendancetxtFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(Attendance_Users.ItemsSource).Refresh();
         }
     }
 }
