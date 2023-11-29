3d4ae52d550a2cdc4832b0cff180c5561ee47b93
tinhpham2004 <boydzpro3032004@gmail.com>
2023-11-29 12:47:18 +0700

    temp save

--- HumanResourcesApplication_DoAn/ViewModel/Admin/EmployeeViewViewModel.cs
+++ HumanResourcesApplication_DoAn/ViewModel/Admin/EmployeeViewViewModel.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using HumanResourcesApplication_DoAn.Model;
+using HumanResourcesApplication_DoAn.Utils;
+using HumanResourcesApplication_DoAn.Views.Employee;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
 
 namespace HumanResourcesApplication_DoAn.ViewModel.Admin
 {
-    internal class EmployeeViewViewModel:ViewModelBase
+    public class EmployeeViewViewModel : ViewModelBase
     {
     }
 }
