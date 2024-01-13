c8b990de6ee0b2a51dada4c7554a3be40f5a185e
Lmt310104 <lmtoan311@gmail.com>
2024-01-13 10:39:34 +0700

    save change

---
+++ HumanResourcesApplication_DoAn/ViewModel/Admin/PayrollMainViewViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourcesApplication_DoAn.ViewModel.Admin
+{
+
+    public class PayrollMainViewViewModel:ViewModelBase
+    {
+        ViewModelBase _currentPayrollChildView;
+        EmployeeAllViewModel _payrollAllViewModel;
+        EmployeeViewViewModel _baseSalaryiewModel;
+
+        public ViewModelBase CurrentPayrollChildView { get => _currentPayrollChildView; set => _currentPayrollChildView = value; }
+        public EmployeeAllViewModel PayrollAllViewModel { get => _payrollAllViewModel; set => _payrollAllViewModel = value; }
+        public EmployeeViewViewModel BaseSalaryiewModel { get => _baseSalaryiewModel; set => _baseSalaryiewModel = value; }
+    }
+
+}
