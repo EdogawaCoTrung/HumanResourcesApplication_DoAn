8ee68c216c74521b948bb394a9ad5c2006c8ee8e
Lmt310104 <lmtoan311@gmail.com>
2024-01-13 10:03:54 +0700

    push code

--- HumanResourcesApplication_DoAn/ViewModel/Admin/ProjectMainViewModel.cs
+++ HumanResourcesApplication_DoAn/ViewModel/Admin/ProjectMainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using HumanResourcesApplication_DoAn.Model;
 using HumanResourcesApplication_DoAn.Repositories;
+using HumanResourcesApplication_DoAn.Views.Admin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,21 @@ namespace HumanResourcesApplication_DoAn.ViewModel.Admin
             listProject = new List<Project>();
             listProjectRepository = new ListProjectRepository();
             listProject = listProjectRepository.ListProject();
+            AddProjectCommand = new ViewModelCommand(ExecuteAddProjectCommand);
         }
+
+        private void ExecuteAddProjectCommand(object? obj)
+
+        {
+            AddProjectViewModel addProjectViewModel = new AddProjectViewModel();   
+            Project_Add project_Add = new Project_Add();
+            project_Add.DataContext = addProjectViewModel;
+            project_Add.ShowDialog();
+        }
+
+        public ViewModelCommand AddProjectCommand { get;}
+  
+        public ViewModelCommand DeleteProjectCommand { get;}
+        public ViewModelCommand EditProjectCommand { get;}
     }
 }
