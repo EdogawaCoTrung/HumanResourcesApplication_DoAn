99b71a8f783488c39eae819da3fef0233ba3bbc1
Lmt310104 <lmtoan311@gmail.com>
2024-01-14 22:09:33 +0700

    save change

--- HumanResourcesApplication_DoAn/Utils/ChangeDate.cs
+++ HumanResourcesApplication_DoAn/Utils/ChangeDate.cs
@@ -16,5 +16,13 @@ namespace HumanResourcesApplication_DoAn.Utils
             string year = date.Year.ToString();
             return year+"-"+month+"-"+day;
         }
+        public string ChangeDateFormatDateOnly(DateOnly date) 
+        {
+            string day = Int32.Parse(date.Day.ToString()) < 10 ? ("0" + date.Day.ToString()) : date.Day.ToString();
+            string month = Int32.Parse(date.Month.ToString()) < 10 ? "0" + date.Month.ToString() : date.Month.ToString();
+            string year = date.Year.ToString();
+            return year + "-" + month + "-" + day;
+        }
+    }
     }
 }
