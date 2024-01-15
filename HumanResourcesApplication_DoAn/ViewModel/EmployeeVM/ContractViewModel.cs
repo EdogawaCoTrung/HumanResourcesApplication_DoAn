55f17fae4ed0de28d1378ffa2fa072a13184d541
Lmt310104 <lmtoan311@gmail.com>
2024-01-15 19:26:44 +0700

    save change

---
+++ HumanResourcesApplication_DoAn/ViewModel/EmployeeVM/ContractViewModel.cs
@@ -0,0 +1,18 @@
+﻿using HumanResourcesApplication_DoAn.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanResourcesApplication_DoAn.ViewModel.EmployeeVM
+{
+    public class ContractViewModel:ViewModelBase
+    {
+        private User _user;
+        public User User { get => _user; set { _user = value; OnPropertyChanged(nameof(User)); } }
+        public ContractViewModel( User currentUser) 
+        {
+            User = currentUser;
+        }    }
+}
---
+++ HumanResourcesApplication_DoAn/Views/Employee/Employee_Contract.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HumanResourcesApplication_DoAn.Views.Employee
+{
+    /// <summary>
+    /// Interaction logic for Employee_Contract.xaml
+    /// </summary>
+    public partial class Employee_Contract : Window
+    {
+        public Employee_Contract()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
